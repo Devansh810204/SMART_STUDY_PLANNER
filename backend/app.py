53fabaeca89446990520b02c0318126b9e17fe13
DEVANSH BANSAL <devanshbansal321@gmail.com>
2025-05-20 11:40:55 +0530

    Add validator import to fix deployment error

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import List, Dict
 from datetime import datetime, timedelta
 import json
